4e4f3dbd6ac5d511414619300baa92761418ef81
Jyotiswarup <jyotijohn305@gmail.com>
2024-03-06 01:01:20 +0530

    Update the javasript to play music and added animation

---
+++ javascript.js
@@ -0,0 +1,66 @@
+// for (let index = 1; index < document.querySelectorAll(".drum").length; index++) {
+//     document.querySelectorAll(".drum")[index].addEventListener("click", function (){
+//         alert("OOPS! Just Clicked");
+//        });}
+
+var numofdrumbuttons = document.querySelectorAll('.drum').length;
+
+for (let index = 0; index < numofdrumbuttons; index++) {
+  document.querySelectorAll('.drum')[index].addEventListener('click', function () {
+    var button = this.innerHTML;
+    audio(button);
+    buttonAnimation(button);
+  });
+}
+
+document.addEventListener('keydown', function (btn) {
+  audio(btn.key);
+  buttonAnimation(btn.key);
+});
+
+function audio(key) {
+  switch (key) {
+    case 'w':
+      var audio = new Audio('sounds/tom-1.mp3');
+      audio.play();
+      break;
+
+    case 's':
+      var audio = new Audio('sounds/tom-2.mp3');
+      audio.play();
+      break;
+    case 'a':
+      var audio = new Audio('sounds/tom-3.mp3');
+      audio.play();
+      break;
+    case 'd':
+      var audio = new Audio('sounds/tom-4.mp3');
+      audio.play();
+      break;
+    case 'j':
+      var audio = new Audio('sounds/snare.mp3');
+      audio.play();
+      break;
+    case 'k':
+      var audio = new Audio('sounds/crash.mp3');
+      audio.play();
+      break;
+
+    case 'l':
+      var audio = new Audio('sounds/kick-bass.mp3');
+      audio.play();
+      break;
+    default:
+      break;
+  }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector('.' + currentKey);
+
+  activeButton.classList.add('pressed');
+
+  setTimeout(function () {
+    activeButton.classList.remove('pressed');
+  }, 100);
+}
